830e524abf07f2b50642198fee29cecab07544e9
kanat <kanttoroev@gmail.com>
2023-10-29 20:40:00 +0300

    fixed whole code for lab6

--- lab6/methods.py
+++ lab6/methods.py
@@ -2,52 +2,44 @@ from tabulate import tabulate
 
 
 def rungeKutte(x0, y0, h, x_last, f, solution, eps=None, log=False, from_main=False):
-    xValues = [x0]
-    yValues = [y0]
+    x_values = [x0]
+    y_values = [y0]
     calculation_table = []
+    lib_solution_table = []
+    while x_values[-1] <= x_last:
+        x = x_values[-1]
+        y = y_values[-1]
 
-    while round(x0 + 3 * h, 5) > x_last:
-        h *= 0.5
-    solution_array = []
-    while xValues[-1] <= x_last:
-        prX = xValues[-1]
-        prY = yValues[-1]
-        k1 = h * f(prX, prY)
-        k2 = h * f(prX + 0.5 * h, prY + 0.5 * k1)
-        k3 = h * f(prX + 0.5 * h, prY + 0.5 * k2)
-        k4 = h * f(prX + h, prY + k3)
-        if prX == x_last:
-            break
-        try:
-            calculation_table.append([len(xValues), prX, k1, k2, k3, k4, prY, f(prX, prY)])
-            solution_array.append(solution([prX, x_last], y0, f)[1])
-        except Exception as ex:
-            print(ex)
-        yValues.append(prY + (1.0 / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4))
-        xValues.append(round(prX + h, 5))
-    calculation_table.append([len(xValues), xValues[-1], k1, k2, k3, k4, yValues[-1], f(xValues[-1], yValues[-1])])
-    solution_array.append(solution([prX, prX + h], y0, f)[1])
-    if eps is not None:  # Проверка точности и изменение шага h
-        h_half = h * 0.5
-        x2h, y2h = rungeKutte(x0, y0, h_half, x_last, f, solution, log=log)
-        R = abs((yValues[-1] - y2h[-1]) / (2 ** 1 - 1))
-        if R > eps:
-            return rungeKutte(x0, y0, h_half, f, solution, eps, log=log, from_main=from_main)
-        else:
-            print(f"По правилу Рунге точность: {round(yValues[-1], 5)} - {round(y2h[-1], 5)} = {round(R, 5)}")
-            # print(f"По правилу Рунге точность: {yValues[-1]} - {(y2h[-1])} = {(R)}")
-            # return xValues, yValues
+        k1 = h * f(x, y)
+        k2 = h * f(x + 0.5 * h, y + 0.5 * k1)
+        k3 = h * f(x + 0.5 * h, y + 0.5 * k2)
+        k4 = h * f(x + h, y + k3)
 
-    for j in range(len(calculation_table)):
-        try:
-            calculation_table[j].append(solution_array[len(calculation_table) - j - 1])
-        except Exception as ex:
-            print(ex)
-    if log and from_main:
-        print(f"Шаг: {h}")
-        field_names = ["i", "x", "k1", "k2", "k3", "k4", "y", "F(x,y)", "Точное решение"]
+        y_next = y + (k1 + 2 * k2 + 2 * k3 + k4) / 6.0
+        x_next = x + h
+        if x == x_last:
+            x -= 1e-10
+        calculation_table.append([len(x_values), x, y, k1, k2, k3, k4, f(x, y)])
+        lib_solution_table.append(solution([x, x_last], y0, f)[1])
+        x_values.append(x_next)
+        y_values.append(y_next)
+
+    if eps is not None:
+        # Расчет значения погрешности по правилу Рунге
+        y_half_step = rungeKutte(x0, y0, h / 2.0, x_last, f, solution, eps=None, log=False, from_main=from_main)[1][-1]
+        R = abs(y_values[-1] - y_half_step) / (2 ** 4 - 1)
+        if R > eps or abs(y_values[-1] - lib_solution_table[0]) > eps:
+            return rungeKutte(x0, y0, h / 2.0, x_last, f, solution, eps=eps, log=log, from_main=from_main)
+
+        if R <= eps and log:
+            print(f"По правилу Рунге точность: {round(y_values[-1], 5)} - {round(y_half_step, 5)} = {round(R, 5)}")
+        for j in range(len(calculation_table)):
+            calculation_table[j].append(lib_solution_table[len(calculation_table) - j - 1])
+    if log:
+        field_names = ["i", "x", "y", "k1", "k2", "k3", "k4", "F(x,y)", "Точное значение"]
         print(tabulate(calculation_table, field_names, tablefmt='grid', floatfmt='2.4f'))
-    return xValues, yValues
+
+    return x_values, y_values
 
 
 def euler(x0, y0, h, x_last, f, solution, eps=None, log=False, from_main=False):
@@ -96,7 +88,6 @@ def euler(x0, y0, h, x_last, f, solution, eps=None, log=False, from_main=False):
             return euler(x0, y0, h_half, x_last, f, solution, eps, from_main=from_main)
         else:
             print(f"По правилу Рунге точность: {round(yValues[-1], 5)} - {round(Y2h[-1], 5)} = {round(R, 5)}")
-            # print(f"Шаг: {h}")
             return xValues, yValues
     if from_main:
         print(f"Шаг: {h}")
@@ -110,20 +101,18 @@ def adams(x0, y0, h, x_last, f, solution, eps=None, log=False, from_main=False):
     yValues = [y0]
     calculation_table = []
     while round(x0 + 3 * h, 5) > x_last:
-        h *= 0.5
+        h *= 0.5  # чтобы избежать выхода за рамки промежутка, т. к. вычисляются первые 4 значения
     # Вычисление начальных значений с помощью метода Рунге-Кутты
-    x, y = rungeKutte(x0, y0, h, x0 + 3 * h, f, solution, eps=None, log=True)
+    x, y = rungeKutte(x0, y0, h, x0 + 3 * h, f, solution, eps=None, log=False)
     yValues.extend(y[1:4])
     xValues.extend(x[1:4])
     fValues = [f(x, y) for x, y in zip(xValues, yValues)]
     solution_array = [solution([x0, x_last], y0, f)[1]]
-    solution_array.append(solution([xValues[1], x_last], y0, f)[1])
-    solution_array.append(solution([xValues[2], x_last], y0, f)[1])
-    solution_array.append(solution([xValues[3], x_last], y0, f)[1])
-    calculation_table.append([1, xValues[0], yValues[0], fValues[0]])
-    calculation_table.append([2, xValues[1], yValues[1], fValues[1]])
-    calculation_table.append([3, xValues[2], yValues[2], fValues[2]])
-    calculation_table.append([4, xValues[3], yValues[3], fValues[3]])
+    for i in range(1, 4):
+        solution_array.append(solution([xValues[i], x_last], y0, f)[1])
+    for i in range(4):
+        calculation_table.append([i + 1, xValues[i], yValues[i], fValues[i], solution_array[i]])
+
     x = round(xValues[-1] + h, 5)
     i = 5
     while x <= x_last:
@@ -140,27 +129,25 @@ def adams(x0, y0, h, x_last, f, solution, eps=None, log=False, from_main=False):
                 9 * new_f + 19 * fValues[-1] - 5 * fValues[-2] + fValues[-3]
         ) / 24
 
-        # Проверка погрешности с использованием правила Рунге
-        if eps is not None and abs(y_corrected - y_predicted) > eps:
-            # Уменьшение шага h
-            h /= 2
-            return adams(x0, y0, h, x_last, f, solution, eps, log)
-
         yValues.append(y_corrected)
         fValues.append(new_f)
-        try:
-            calculation_table.append([i, x, y_corrected, new_f])
-            solution_array.append(solution([x, x_last], y0, f)[1])
-        except IndexError as ex:
-            solution_array.append(solution([x, x_last + h], y0, f)[1])
-            print(ex)
+        if x == x_last:
+            x -= 1e-10
+        calculation_table.append([i, x, y_corrected, new_f])
+        solution_array.append(solution([x, x_last], y0, f)[1])
         x = round(x + h, 5)
         i += 1
-    for j in range(len(calculation_table)):
-        try:
-            calculation_table[j].append(solution_array[len(calculation_table) - j - 1])
-        except IndexError as ex:
-            print(ex)
+
+    for i in range(4, len(calculation_table)):
+        calculation_table[i].append(solution_array[len(calculation_table) - i - 1])
+    if eps is not None:
+        h_half = h / 2.0
+        x2h, Y2h = adams(x0, y0, h_half, x_last, f, solution)
+        R = abs((yValues[-1] - Y2h[-1]) / (2 ** 4 - 1))
+
+        if R > eps or abs(yValues[-1] - solution_array[0]) > eps:
+            return adams(x0, y0, h_half, x_last, f, solution, eps, log=log, from_main=from_main)
+        print(f"По правилу Рунге точность: {round(yValues[-1], 5)} - {round(Y2h[-1], 5)} = {round(R, 5)}")
 
     if log:
         field_names = ["i", "x", "y", "F(x,y)", "Точное решение"]
